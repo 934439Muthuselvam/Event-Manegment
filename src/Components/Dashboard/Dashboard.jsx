6984618b79666b4ffa56b6887153e00157de943d
vickyArumugam <vickyat2162000@example.com>
2024-12-19 00:07:33 +0530

    vicky

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Dashboard() {
   return (
     <div>
-       <h1 className="font-bold mb-10 text-2xl"> </h1>
+       <h1 className="font-bold mb-10 text-2xl">dash</h1>
     </div>
   )
 }
+
+// hello
